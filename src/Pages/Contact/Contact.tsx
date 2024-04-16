5c5d2c69f24704a45a12143e73581cb2dab93d5d
Sahil1491 <Jaswalsahil1491@gmail.com>
2024-04-16 17:52:38 +0530

    Contact changes

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,5 +1,7 @@
 import './Contact.css'
 import Footer from '../../Components/Footer/Footer';
+import sideImg1 from '../../Components/Assests/SideImg1.webp';
+import sideImg2 from '../../Components/Assests/SideImg2.webp';
 
 export default function Constact() {
   return (
@@ -16,6 +18,28 @@ export default function Constact() {
         </p>
        
      </div>
+     
+      {/* New div with image and content */}
+      <div className="ImageWithContent">
+        <div className="ImageContainer">
+          <img src={sideImg1} alt="Description" />
+        </div>
+        <div className="ContentContainer">
+          <p>
+            "Shopping Cart is an exceptional ecommerce platform, renowned for its top-notch service and customer satisfaction. It excels in providing a seamless shopping experience, offering a wide range of products, secure transactions, and prompt delivery. Its user-friendly interface and responsive support make it the preferred choice for online shoppers worldwide."</p>
+        </div>
+      </div>
+
+      <div className="ImageWithContent">
+        <div className="ImageContainer">
+          <img src={sideImg2} alt="Description" />
+        </div>
+        <div className="ContentContainer">
+
+         <p> "Shopping Cart, founded in 2024 by Sahil, revolutionizes online shopping through its innovative use of React.js. This cutting-edge platform offers seamless navigation, real-time updates, and personalized experiences. Its robust architecture ensures secure transactions and efficient order management, making it a preferred choice for modern e-commerce solutions."</p>
+
+        </div>
+      </div>
      <Footer/>
     </>
   )
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -11,8 +11,7 @@ import LaptopImg from '../../Components/Assests/Laptop.jpeg';
 import Slide2 from '../../Components/Assests/Slide2.jpeg';
 import Slide4 from '../../Components/Assests/this-is-same-shoes_329181-1769.avif';
 import Footer from '../../Components/Footer/Footer';
-import sideImg1 from '../../Components/Assests/SideImg1.webp';
-import sideImg2 from '../../Components/Assests/SideImg2.webp';
+
 
 interface ProductItem {
   name: string;
@@ -90,30 +89,6 @@ function Home({ setCartCount }: { setCartCount: React.Dispatch<React.SetStateAct
           </div>
         </div>
       </div>
-
-      {/* New div with image and content */}
-      <div className="ImageWithContent">
-        <div className="ImageContainer">
-          <img src={sideImg1} alt="Description" />
-        </div>
-        <div className="ContentContainer">
-          <p>
-            "Shopping Cart is an exceptional ecommerce platform, renowned for its top-notch service and customer satisfaction. It excels in providing a seamless shopping experience, offering a wide range of products, secure transactions, and prompt delivery. Its user-friendly interface and responsive support make it the preferred choice for online shoppers worldwide."</p>
-        </div>
-      </div>
-
-      <div className="ImageWithContent">
-        <div className="ImageContainer">
-          <img src={sideImg2} alt="Description" />
-        </div>
-        <div className="ContentContainer">
-
-         <p> "Shopping Cart, founded in 2024 by Sahil, revolutionizes online shopping through its innovative use of React.js. This cutting-edge platform offers seamless navigation, real-time updates, and personalized experiences. Its robust architecture ensures secure transactions and efficient order management, making it a preferred choice for modern e-commerce solutions."</p>
-
-        </div>
-      </div>
-
-
       <Footer />
     </>
   );
